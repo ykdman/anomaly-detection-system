55fadcfdc475f77651ef557e8fd97a705114d360
YKDman <dbsrudejr300@gmail.com>
2025-09-16 23:31:21 +0900

    inital Commit : 20250916

---
+++ src/main/java/com/ads/anomalydetectionsystem/AnomalyDetectionSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ads.anomalydetectionsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnomalyDetectionSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnomalyDetectionSystemApplication.class, args);
+    }
+
+}
